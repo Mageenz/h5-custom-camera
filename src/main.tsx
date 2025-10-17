6e2eb6c33a1ec69e2569f6b5836336dc43a07cde
machao1006 <clovemx@gmail.com>
2025-10-17 14:24:42 +0800

    style

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import Camera from './Camera.tsx'
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/camera" element={<Camera />} />
+      </Routes>
+    </Router>
   </StrictMode>,
 )
